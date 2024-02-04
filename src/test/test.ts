20518ad8cf67a74392dbbfa109d92b1f6127a00e
wangkai <wang_kai_1998@163.com>
2024-02-04 16:17:39 +0800

    修改测试例

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,7 +1,7 @@
 import { printTreeByArray } from "utils";
 
 function test() {
-  const treeNums: number[] = [1, 2, 3, 4, 5, 6];
+  const treeNums: (number | null)[] = [1, 2, 3, 4, 5, null, 6];
   printTreeByArray(treeNums);
 }
 
